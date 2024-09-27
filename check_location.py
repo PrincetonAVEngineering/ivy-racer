477dd2c6702c0d9773518330e4f85220d4aa1671
Alexei Korolev <124002470+AlexeiKorolev@users.noreply.github.com>
2024-09-26 20:06:03 -0400

    API key remove check_location.py

--- check_location.py
+++ check_location.py
@@ -1,10 +1,12 @@
 import requests
 from find_location import get_coordinates
 
+API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
+
 location = get_coordinates()
 lat = location[0]
 long = location[1]
-url = 'https://roads.googleapis.com/v1/nearestRoads?points='+str(lat)+'%2C'+str(long)+'&key=AIzaSyAbxTKktcXZ0iNWrJi_xQwtTikAJrkrfE4'
+url = 'https://roads.googleapis.com/v1/nearestRoads?points={lat}%2C{long}&key={API_KEY}'
 print(lat)
 print(long)
 r = requests.get(url, auth=('user', 'pass'))
