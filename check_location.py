3bf44aec74e627b84011d1f8a198ffbbd2a54266
mpineroj <mpineroj@gmail.com>
2024-10-03 18:12:07 -0400

    finished check_location

--- check_location.py
+++ check_location.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from find_location import get_coordinates
 
 API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
@@ -6,10 +7,15 @@ API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 location = get_coordinates()
 lat = location[0]
 long = location[1]
-url = 'https://roads.googleapis.com/v1/nearestRoads?points={lat}%2C{long}&key={API_KEY}'
+url = f'https://roads.googleapis.com/v1/nearestRoads?points={lat}%2C{long}&key={API_KEY}'
 print(lat)
 print(long)
 r = requests.get(url, auth=('user', 'pass'))
 
 results = r.json()
-print(results["snappedPoints"][0])
+print(results["snappedPoints"][0]["location"])
+
+new_long, new_lat = results["snappedPoints"][0]["location"]["longitude"], results["snappedPoints"][0]["location"]["latitude"]
+
+print(new_lat)
+print(new_long)
