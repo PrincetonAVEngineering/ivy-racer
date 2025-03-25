945e5b2ad99672b657901bf5f348b07fa01289a2
ScopeStudios <alexeithepro@gmail.com>
2025-03-25 15:13:25 -0400

    init

--- blinders.py
+++ blinders.py
@@ -5,25 +5,28 @@ import numpy as np
 model = tf.keras.models.load_model("lane_detection1.keras")
 
 # Open the default camera
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture("Bike Path.mp4")
+# cam = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cam.read()
-
-    print(type(frame))
+    if not ret:
+        break
         
-    input = cv2.resize(frame, (512,512))
+    frame_resized = cv2.resize(frame, (512,512)) / 255.0
+    input = np.expand_dims(frame_resized, axis=0)
     
-    input = np.expand_dims(input, axis=0)
     result = model(input).numpy()
-    result = np.asarray(result, 'float32')
-
-    print(type(result))
-
-    cv2.imshow('Camera', result)
+    result = np.squeeze(result)  # Remove batch dimension
+    
+    # Convert to uint8 format and ensure proper range (0-255)
+    result = (result * 255).astype(np.uint8)
+    
+    cv2.imshow('Processed', result)
+    cv2.imshow("Original", frame_resized)
 
     # Press 'q' to exit the loop
-    if cv2.waitKey(1) == 'q':
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release the capture
